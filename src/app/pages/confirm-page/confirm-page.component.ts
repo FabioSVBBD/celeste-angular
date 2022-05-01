687e19c3355d920a0dd83de62acbef62b797a72a
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-01 19:30:48 +0200

    Connected buttons on Confirm Page

--- src/app/pages/confirm-page/confirm-page.component.ts
+++ src/app/pages/confirm-page/confirm-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 import { state } from '../../../assets/state/state';
 
 @Component({
@@ -9,21 +11,36 @@ import { state } from '../../../assets/state/state';
 export class ConfirmPageComponent implements OnInit {
   total: number;
 
-  constructor() {
-    this.total = Object.values(state).reduce(
+  constructor(private router: Router, private location: Location) {
+    this.total = this.getTotal();
+  }
+
+  getState(): {
+    [key: string]: { name: string; quantity: number; amount: number };
+  } {
+    return state;
+  }
+
+  getTotal(): number {
+    return Object.values(state).reduce(
       (total, current) => total + current.quantity * current.amount,
       0
     );
   }
 
-  getState() {
-    return state;
-  }
-
-  updateQuantityOf(quantity: number, key: string) {
+  updateQuantityOf(quantity: number, key: string): void {
     console.log('quantity updated for', key, ':', quantity);
 
     state[key].quantity = quantity;
+    this.total = this.getTotal();
+  }
+
+  menuClicked(): void {
+    this.location.back();
+  }
+
+  confirmOrderClicked(): void {
+    this.router.navigate(['summary']);
   }
 
   ngOnInit(): void {}
