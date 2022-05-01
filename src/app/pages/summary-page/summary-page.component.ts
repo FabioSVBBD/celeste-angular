cb24fd25c82f849c09b7113b589c7fde908a31c9
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-01 20:51:04 +0200

    Created Summary Page

--- src/app/pages/summary-page/summary-page.component.ts
+++ src/app/pages/summary-page/summary-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { state } from '../../../assets/state/state';
+
+const tastingMenu = 'tastingMenu';
 
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
-  styleUrls: ['./summary-page.component.css']
+  styleUrls: ['./summary-page.component.css'],
 })
 export class SummaryPageComponent implements OnInit {
+  hasTastingData: boolean = true;
+  hasCarteData: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  getTastingData(): { name: string; quantity: number; amount: number } {
+    return state[tastingMenu];
   }
 
+  // rewrite this (map or reduce)
+  getCarteData(): { name: string; amount: number; quantity: number }[] {
+    let retVal: { name: string; amount: number; quantity: number }[] = [];
+
+    Object.keys(state).forEach((key) => {
+      if (key !== tastingMenu) {
+        retVal.push(state[key]);
+      }
+    });
+
+    return retVal;
+  }
+
+  getTotal(): number {
+    return Object.values(state).reduce(
+      (total, current) => total + current.quantity * current.amount,
+      0
+    );
+  }
+
+  payClicked() {
+    console.log('Pay with Vodapay clicked', state);
+  }
+
+  ngOnInit(): void {}
 }
