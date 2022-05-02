49c5c7afc24d724c3f0212ca21d6a6eb4c1ccacb
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-02 12:27:23 +0200

    Connected Buttons on A la Carte home

--- src/app/pages/carte-journey/home/home.component.ts
+++ src/app/pages/carte-journey/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { menu } from 'src/assets/data/tasting-menu';
+
+const menuOptions = {
+  starters: 'Starters',
+  mains: 'Mains',
+  desserts: 'Desserts',
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  menuOptions() {
+    return menuOptions;
   }
 
+  viewOrderClicked() {
+    this.router.navigate(['confirm']);
+  }
+
+  menuClicked(type: string) {
+    console.log(`routing to /a-la-carte/${type}`);
+  }
 }
