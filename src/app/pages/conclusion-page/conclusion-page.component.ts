3a241dd574d4cc5f5bb9b34beb8cc100ce48941a
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-02 11:19:22 +0200

    Completed Tasting Journey

--- src/app/pages/conclusion-page/conclusion-page.component.ts
+++ src/app/pages/conclusion-page/conclusion-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conclusion-page',
   templateUrl: './conclusion-page.component.html',
-  styleUrls: ['./conclusion-page.component.css']
+  styleUrls: ['./conclusion-page.component.css'],
 })
 export class ConclusionPageComponent implements OnInit {
+  table: number = 317;
+  user: { name: string; email: string } = {
+    name: 'Athena',
+    email: 'athenaharkness@yahoo.com',
+  };
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  homeClicked() {
+    this.router.navigate(['']);
   }
-
 }
