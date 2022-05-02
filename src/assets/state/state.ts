d7ced7a5698f253e8e22b77c0c8e3c112c504393
Fabio Sousa Vieira <fabio.sousavieira@vcontractor.co.za>
2022-05-02 15:32:44 +0200

    Removed stub state

--- src/assets/state/state.ts
+++ src/assets/state/state.ts
@@ -1,7 +1,3 @@
 export const state: {
   [key: string]: { name: string; quantity: number; amount: number };
-} = {
-  tastingMenu: { name: 'Tasting Menu', quantity: 2, amount: 100 },
-  // chicken: { name: 'Chicken', quantity: 1, amount: 120 },
-  // pasta: { name: 'Pasta', quantity: 1, amount: 115 },
-};
+} = {};
